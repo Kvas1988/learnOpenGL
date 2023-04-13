1b3b952c9c04494bd3f3e8bcec10cae5e0c030f0
Kvas <shokhin.al@gmail.com>
2023-04-13 12:46:15 +0300

    glfw ver setup

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use opengl
+  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  return 0;
 }
